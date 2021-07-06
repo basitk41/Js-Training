bc2b8b3f2a04447b008f771ac762d813982636fd
Basit Ali <basitk41@gmail.com>
2021-07-06 20:23:54 +0500

    Chapter 1 added

---
+++ assets/chapters/chapter1.js
@@ -0,0 +1,42 @@
+// single line comment
+/* 
+multiple lines comments.
+*/
+
+// case sensitive
+let a = 1;
+let A = 2;
+console.log(a);
+console.log(A);
+
+// iddentifiers
+let firstName_123 = "Khan";
+
+// printing hello world
+console.log("Hello world");
+//document.write("Hello world");
+//alert("Hello world");
+//document.getElementById("id").value = "Hello world";
+
+// variables
+// var a;
+// let b;
+// const c;
+
+// datatypes
+// let a="a"
+// let b= 2;
+// let c = false;
+// let d = null;
+
+// premitive type
+// let a=2;
+// let b=a;
+// reference
+// let a = [];
+// let b=a;
+
+// expression
+console.log(2 * 2 + 2);
+// keywords
+let delete1 = "a";

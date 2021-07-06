7530472f24bdaa36eaa1106d9c476404a99cafbf
Basit Ali <basitk41@gmail.com>
2021-07-06 20:24:03 +0500

    Chapter 2 added

---
+++ assets/chapters/chapter2.js
@@ -0,0 +1,49 @@
+// variables declaration
+var a = "a";
+let b = "b";
+const c = "c";
+
+{
+  {
+    var d = "d";
+  }
+  //   console.log(d);
+}
+// console.log(d);
+// {
+//   {
+//     const e = "e";
+//     e = "something";
+//     console.log(e);
+//   }
+// }
+// console.log(d);
+
+// unddefined and undeclerd
+// let z;
+// console.log(z);
+// console.log(x);
+
+//Global vs Local
+// let Global = 1;
+// {
+//   let Local = 2;
+//   console.log(Global);
+// }
+// console.log(loc);
+
+// non strict mood
+// {
+//   s = 10;
+//   console.log("1st", s);
+// }
+// console.log("2nd", s);
+
+//hoisting
+// console.log(l);
+// let l;
+
+// hi();
+// function hi() {
+//   console.log("hi");
+// }
